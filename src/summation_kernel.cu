4c429e6250a5a72c61e5c5c24ea9632d6e48803f
melian ferrachat <18008276@e008m01.istic.univ-rennes1.fr>
2018-03-21 09:52:22 +0100

    Q5

--- src/summation_kernel.cu
+++ src/summation_kernel.cu
@@ -2,9 +2,14 @@
 // GPU kernel
 __global__ void summation_kernel(int data_size, float * data_out)
 {
+	int tid = blockIdx.x * blockDim.x +threadIdx.x;
+	int data_chunk = data_size / (blockDim.x * gridDim.x);
+	
 	float result = 0.0;
-	for(int i = ; i >= ; i--)
-		*data_out = i%2 ? result-1.0/(i+1) : result+1.0/(i+1);
+	
+	for(int i = data_chunk*(tid+1); i >= data_chunk*tid; i--)
+		result = i%2 ? result-1.0/(i+1) : result+1.0/(i+1);
+	*data_out = result;
 }
 
 
