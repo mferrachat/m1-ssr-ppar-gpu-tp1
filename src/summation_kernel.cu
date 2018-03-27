191130591a909b26c35c3031dddfaa56fde811a2
mferrachat <melian.ferrachat@gmail.com>
2018-03-27 17:41:12 +0200

    Fix des bornes en fonction du positif/négatif.

--- src/summation_kernel.cu
+++ src/summation_kernel.cu
@@ -8,7 +8,7 @@ __global__ void summation_kernel(int data_size, float * data_out)
 	float result = 0.0;
 	
 	if(tid%2)
-		for(int i = data_chunk*2*(tid+1)+1; i >= data_chunk*2*tid+1; i-=2)
+		for(int i = data_chunk*2*tid+1; i >= data_chunk*2*(tid-1)+1; i-=2)
 			result = result-1.0/(i+1);
 	else
 		for(int i = data_chunk*2*(tid+1); i >= data_chunk*2*tid; i-=2)
