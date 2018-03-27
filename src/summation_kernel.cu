572d084b5f4fc67bec083da8bee36bfcb64fcf1b
mferrachat <melian.ferrachat@gmail.com>
2018-03-27 17:33:39 +0200

    Solution basé sur le partage addition/soustraction des blocs.

--- src/summation_kernel.cu
+++ src/summation_kernel.cu
@@ -2,13 +2,17 @@
 // GPU kernel
 __global__ void summation_kernel(int data_size, float * data_out)
 {
-	int tid = blockIdx.x * blockDim.x +threadIdx.x;
+	int tid = blockIdx.x * blockDim.x + threadIdx.x;
 	int data_chunk = data_size / (blockDim.x * gridDim.x);
 	
 	float result = 0.0;
 	
-	for(int i = data_chunk*(tid+1); i >= data_chunk*tid; i--)
-		result = i%2 ? result-1.0/(i+1) : result+1.0/(i+1);
+	if(tid%2)
+		for(int i = data_chunk*2*(tid+1)+1; i >= data_chunk*2*tid+1; i-=2)
+			result = result-1.0/(i+1);
+	else
+		for(int i = data_chunk*2*(tid+1); i >= data_chunk*2*tid; i-=2)
+			result = result+1.0/(i+1);
 	data_out[tid] = result;
 }
 
